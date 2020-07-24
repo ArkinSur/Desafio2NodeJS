0b13a677018d6aa674170ed870e500cdd60b5570
Filipe Drummond <exgavian82@gmail.com>
2020-07-24 03:53:48 -0300

    :rocket: Finish all routes

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,10 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +19,47 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncomeTransactions = this.transactions.filter(
+      item => item.type === 'income',
+    );
+    const array1: number[] = [0];
+    totalIncomeTransactions.forEach(item => {
+      array1.push(item.value);
+    });
+    const totalIncome = array1.reduce((prevItem, item) => {
+      return prevItem + item;
+    });
+
+    const totalOutcomeTransactions = this.transactions.filter(
+      item => item.type === 'outcome',
+    );
+
+    const array2: number[] = [0];
+    totalOutcomeTransactions.forEach(item => {
+      array2.push(item.value);
+    });
+    const totalOutcome = array2.reduce((prevItem, item) => {
+      return prevItem + item;
+    });
+
+    const balance: Balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
